ef6685bb1552d7df11be0b0d645b3354b4570936
Josh Wygle <jwygle@gmail.com>
2019-09-29 19:10:59 -0400

    Refactored code from Program.cs to ProductPriceHistory.cs

--- TestEppApp/AppSettings.cs
+++ TestEppApp/AppSettings.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace TestEppApp
 {
     public static class AppSettings
     {
-        public static string DefaultConnection
+        private static string DefaultConnection
         {
             get => "Server=.\\SQLEXPRESS;Database=AdventureWorks2016CTP3;Trusted_Connection=True;MultipleActiveResultSets=true;";
         }
 
-        
+        private static string RemoteConnection
+        {
+            get => "Server=JYOSSHU\\SQLEXPRESS,49172;Database=AdventureWorks2016CTP3;User Id=jwygle_macos;Password=dFSYrnIWjjteW8sQeaCe;";
+        }
+
+        private static bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        public static string ConnectionString()
+        {
+            if (isWindows == true)
+            {
+                return DefaultConnection;
+            }
+            else
+            {
+                return RemoteConnection;
+            }
+        }
     }
 }
---
+++ TestEppApp/ProductPriceHistory.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using OfficeOpenXml;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace TestEppApp
+{
+    public static class ProductPriceHistory
+    {
+        public static void CreateProductPriceHistory()
+        {
+            Utils.OutputDir = new DirectoryInfo($"{AppDomain.CurrentDomain.BaseDirectory}SampleApp");
+
+            var file = Utils.GetFileInfo("JoshTest.xlsx");
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Products Price History");
+
+                if (worksheet != null)
+                {
+                    const int headerRow = 1;
+                    const int startRow = 3;
+                    int row = startRow;
+                    int totCol = 0;
+
+
+                    using (SqlConnection sqlConn = new SqlConnection(AppSettings.ConnectionString()))
+                    {
+                        sqlConn.Open();
+                        using (SqlCommand sqlCmd = new SqlCommand(query, sqlConn))
+                        {
+                            using (SqlDataReader sqlReader = sqlCmd.ExecuteReader())
+                            {
+                                while (sqlReader.Read())
+                                {
+                                    int col = 1;
+                                    totCol = sqlReader.FieldCount;
+
+                                    for (int i = 0; i < sqlReader.FieldCount; i++)
+                                    {
+                                        if (sqlReader.GetValue(i) != null)
+                                        {
+                                            if (row - 2 == headerRow)
+                                            {
+                                                worksheet.Cells[headerRow, col].Value = sqlReader.GetName(i);
+                                            }
+                                            worksheet.Cells[row, col].Value = sqlReader.GetValue(i);
+
+                                        }
+
+                                        col++;
+                                    }
+                                    row++;
+                                }
+                                sqlReader.Close();
+
+                                worksheet.Cells[headerRow, 1, headerRow, totCol].Style.Font.Bold = true;
+                                worksheet.Cells[headerRow, 1, headerRow, totCol].Style.Border.Bottom.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                                worksheet.Cells[startRow, 8, row - 1, 8].Style.Numberformat.Format = "YYYY-MM-DD";
+                                worksheet.Cells[startRow, 9, row - 1, 9].Style.Numberformat.Format = "YYYY-MM-DD";
+                            }
+                        }
+                        sqlConn.Close();
+                    }
+                }
+
+                excelPackage.Workbook.Properties.Title = "Josh Test - Product Cost History";
+                excelPackage.Workbook.Properties.Author = "Josh Wygle";
+
+                excelPackage.Save();
+            }
+        }
+
+        static readonly string query = @"Select prod.Name AS [Product Name]
+, prod.ProductNumber AS [Product Number]
+, prod.Color AS [Color]
+, prod.StandardCost AS [Cost]
+, prod.ListPrice AS [Current List Price]
+, ISNULL(prod.Size, 'N/A') AS [Size]
+, ISNULL(prod.SizeUnitMeasureCode, 'N/A') AS [Size UoM]
+, listHis.StartDate AS [Start Date]
+, listHis.EndDate AS [End Date]
+, listHis.ListPrice [List Price]
+From Production.Product prod
+Inner Join Production.ProductListPriceHistory listHis On listHis.ProductID = prod.ProductID
+Order By prod.ProductID, listHis.StartDate Asc";
+    }
+}
+
